e5c61350c9d031508cc1f00adec1e28fd421c644
Simon Gustafsson <simon.gustafsson@iteam.se>
2024-09-14 18:40:06 +0200

    refactor: Remove unused PR data initialization code

--- lib/pr-data.ts
+++
@@ -1,9 +0,0 @@
-import { PR } from "@/types/PR";
-import { generatePRData } from "@/lib/gpt4-pr-generator";
-
-export let prData: PR[] = [];
-
-export async function initializePRData() {
-  const prData = await generatePRData();
-  return prData;
-}
