75a563e71f1954e303f7969e42be159504aaeff3
Simon Gustafsson <simon.gustafsson@iteam.se>
2024-09-17 20:31:45 +0200

    Refactor PR route to remove unused code and improve performance

--- app/api/pr/route.ts
+++ app/api/pr/route.ts
@@ -1,28 +1,5 @@
 import { NextResponse } from "next/server";
 import clientPromise from "@/lib/mongodb";
-import { generatePRData } from "@/lib/gpt";
-
-export async function POST() {
-  try {
-    const client = await clientPromise;
-    const database = client.db("just-merge");
-    const collection = database.collection("PRs");
-    const iterations = 30;
-    const prData = await Promise.all(
-      Array.from({ length: iterations }, () => generatePRData())
-    ).then((results) => results.flat());
-    await collection.insertMany(prData);
-    return NextResponse.json(
-      { message: "PRs added successfully", prData },
-      { status: 200 }
-    );
-  } catch (error) {
-    return NextResponse.json(
-      { message: "Failed to add PRs", error: (error as Error).message },
-      { status: 500 }
-    );
-  }
-}
 
 export async function GET(req: Request) {
   try {
